014779340a3a9d0231353b88ef538654147f86ca
JesperSolutions <jespersolutions@gmail.com>
2026-03-10 16:07:09 +0100

    fix: add Prisma Accelerate extension and directUrl for Prisma Postgres

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 // Prevent multiple PrismaClient instances in development (hot reload)
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: ReturnType<typeof makePrisma> }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+function makePrisma() {
+  return new PrismaClient().$extends(withAccelerate())
+}
+
+export const prisma = globalForPrisma.prisma ?? makePrisma()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
